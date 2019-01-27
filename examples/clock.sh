5145b5defff4960d628c2851df8c7fa086930f75
William Beebe <wbeebe@gmail.com>
2019-01-27 09:36:36 -0500

    Cleanups to main license and two bash scripts.

--- examples/clock.sh
+++ examples/clock.sh
@@ -1,8 +1,25 @@
 #!/bin/bash
 #
-# A simple clock that uses the Bash shell to send the Unix date and time to a pair of
-# Adafruit displays. CTRL C is trapped to turn off the displays before exiting from
-# the script.
+# Copyright (c) 2019 William H. Beebe, Jr.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# ------------------------------------------------------------------------------
+#
+# A simple clock that uses the Bash shell to send the Unix date and time to a
+# pair of Adafruit displays.
+#
+# CTRL C is trapped to turn off the displays before exiting from the script.
 #
 trap ctrl_c INT
 
@@ -11,15 +28,19 @@ function ctrl_c() {
     exit
 }
 #
-# Get the date from the Unix date command, using +%r to get the time formatted in the
-# locale's time format. This will give it in 12 hour format, with an AM or PM.
-# Then use the Bash shell's built-in regex to replace the last ':' and second digits
-# with a space, so that the time string is in the format '12:00 PM' and will print
-# across the eight Adafruit hexadecimal digits.
+# Get the date from the Unix date command, using +%r to get the time formatted
+# in the locale's time format. This will give it in 12 hour format, with an
+# AM or PM. Then use the Bash shell's built-in regex to replace the last ':'
+# and second digits with a space, so that the time string is in the format
+# '12:00 PM' and will print across the eight Adafruit hexadecimal digits.
 #
 while true; do
     DATE=$(date +%r)
-    display print "${DATE//:[0-9][0-9] / }" > /dev/null;
-    sleep 1;
+    display print "${DATE//:[0-9][0-9] / }" > /dev/null
+    sleep 3
+    DATE=$(date +%D)
+    display print "$DATE" > /dev/null
+    sleep 3
+    display clear > /dev/null
 done
 
--- shell/rpinfo.sh
+++ shell/rpinfo.sh
@@ -1,14 +1,31 @@
 #!/usr/bin/env bash
+#
+# Copyright (c) 2019 William H. Beebe, Jr.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# ------------------------------------------------------------------------------
 # 
-# Simple Bash script to display information about any Raspberry Pi board in inventory.
+# Simple Bash script to display information about any Raspberry Pi board in
+# inventory.
 #
-# Note that for the version string we have to use tr to substitute the ending null byte
-# for a newline, or else a warning about null is emitted by the latest versions
-# of Bash.
+# Note that for the version string we have to use tr to substitute the ending
+# null byte for a newline, or else a warning about null is emitted by the
+# latest versions of Bash.
 #
-version=$(cat /proc/device-tree/model | tr '\0' '\n')
+model=$(cat /proc/device-tree/model | tr '\0' '\n')
 echo
-echo " ${version}"
+echo " ${model}"
 echo
 
 memTotal=$(cat /proc/meminfo | grep MemTotal | sed 's/:/ :/' | sed 's/  */ /g')
@@ -24,6 +41,11 @@ echo "   ${hardware}"
 
 revision=$(cat /proc/cpuinfo | grep Revision | tr '\t' ' ')
 echo "   ${revision}"
+echo
+kernelRevision=$(uname -r)
+echo "   Kernel Release: ${kernelRevision}"
+description=$(lsb_release --all 2>/dev/null | grep Description | tr '\t' ' ')
+echo "   OS ${description}"
 echo
 
 echo " Languages Installed"
@@ -52,6 +74,8 @@ version=$(python -V 2>&1)
 echo " ${version}"
 version=$(python3 -V)
 echo " ${version}"
+version=$(pip3 -V)
+echo " (pip3) ${version}"
 
 version=$(gcc --version)
 wordArray=(${version})
