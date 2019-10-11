28b644a46673e36cf6e88389e1eaa5079862c9d4
wbeebe@gmail.com <wbeebe@gmail.com>
2019-10-10 22:21:33 -0400

    Cleanup and consistency of variables.

--- tutorials/cmake/Tutorial/Tutorial.cpp
+++ tutorials/cmake/Tutorial/Tutorial.cpp
@@ -1,4 +1,6 @@
 // A simple program that computes the square root of a number
+// using a modification of Newton's method
+//
 #include <algorithm>
 #include <iostream>
 #include <sstream>
@@ -13,7 +15,7 @@ int main (int argc, char *argv[]) {
     // Extract the name of the executable, which is the base file name.
     //
     std::string argv0(argv[0]);
-    std::string base_filename = argv0.substr(argv0.find_last_of("/") + 1);
+    std::string baseFilename = argv0.substr(argv0.find_last_of("/") + 1);
     //
     // Use regular expressions, via Boost::regex, to make sure it's a valid number.
     //
@@ -21,8 +23,8 @@ int main (int argc, char *argv[]) {
     bool isNumeric = argv[1] != NULL && boost::regex_match(argv[1], expr);
 
     if (!isNumeric) {
-        std::cout << base_filename << " Version " << Tutorial_VERSION_MAJOR << "." << Tutorial_VERSION_MINOR <<std::endl;
-        std::cout << "Usage: " << base_filename << " number" << std::endl;
+        std::cout << baseFilename << " Version " << Tutorial_VERSION_MAJOR << "." << Tutorial_VERSION_MINOR <<std::endl;
+        std::cout << "Usage: " << baseFilename << " number" << std::endl;
         return 1;
     }
 
